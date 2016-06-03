d29657545c5e851bd4faf7b457366d69f2fd51eb
jingjing521 <1353281097@qq.com>
2016-06-03 13:02:33 +0800

    qq

---
+++ scripts/database.js
@@ -0,0 +1,1 @@
+var database=[{"name":"xy2-002长安","src":"./database/02.长安.MP3","artisan":"网易","duration":"1:43"},{"name":"xy2-001大唐境内","src":"./database/01.大唐境内.MP3","artisan":"网易","duration":"2:16"}]
